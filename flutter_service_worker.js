1165bdab1e149c1f8c31bc36249dfd10fa58a07d
Ruan Porto Marques <marques.ruan@gmail.com>
2023-04-25 09:13:49 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "ff7b66e35f24240d878c5401d71dddd8",
-"index.html": "3727037459896010675317090c2f870f",
-"/": "3727037459896010675317090c2f870f",
-"main.dart.js": "890bb6c181cebd6b322a739642cd9ac3",
+"index.html": "4df4b7fafcc08ec64741c58d8b3a3bc6",
+"/": "4df4b7fafcc08ec64741c58d8b3a3bc6",
+"main.dart.js": "01753dd24c2b17b763a1c364219eec4a",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
